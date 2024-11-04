4a7889cf972a1547b21818fd5fd841747e7069c6
JustChad1 <ramosrichard176@gmail.com>
2024-11-04 18:16:52 +0800

    initial commit

---
+++ classSubjects.js
@@ -0,0 +1,8 @@
+let subjectTitle = "Data Structures and Algorithms";
+let classSchedule = "9:30-10:30-AM & 1-2:30-PM / Monday&Friday:";
+let classroom = "Computer Laboratory A";
+let classInstructor = "Mr. Vitruvius John Barayuga";
+let studentName = "Richard Michael V. Ramos";
+
+// Log it to the console//
+console.log('${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}');
